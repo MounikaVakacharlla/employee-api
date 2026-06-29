83c65a2f5fd86a2910dec029ac4634cbd9ea7f47
mounikavakacharlla <vakacharllamounika1014@gmail.com>
2026-06-29 06:57:51 +0000

    Added Django unit tests

--- employee/tests.py
+++ employee/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+
 
 # Create your tests here.
+
+from django.test import TestCase
+
+
+class EmployeeTest(TestCase):
+
+    def test_addition(self):
+        self.assertEqual(10 + 20, 30)
+
+    def test_string(self):
+        self.assertEqual("employee".upper(), "EMPLOYEE")
